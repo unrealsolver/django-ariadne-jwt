cf657325b0c46967150b8937b8989abaa22d15f7
Ruslan <Ruslan.Panasiuk@gmail.com>
2021-01-25 17:50:53 +0300

    Require Pyjwt up to v2.0.0
    PyJWT 2+ is currently incompatible with django-ariadne-jwt

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     install_requires=[
         'ariadne>=0.6.0',
         'Django>=2.0.0',
-        'PyJWT>=1.5.0',
+        'PyJWT>=1.5.0,<2.0.0',
     ],
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
